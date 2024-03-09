b27e135c0e28bd1533e2e2fff993f378f2a2956b
Vitaliy Zhalalov <zhalalov9@mail.ru>
2024-03-09 18:30:49 +0300

    edit models.py

--- models.py
+++ models.py
@@ -6,21 +6,63 @@ from sqlalchemy.sql import func
 from database import Base, db_session, engine as db_engine
 import datetime
 
-class restaurants(Base):
-    __tablename__ = 'restraurants'
-    id = Column(Integer, primary_key=True, default="")
-    restaurant = Column(String(25), nullable=False, default="")
-    rating = Column(Integer, nullable=False)
-    feedback = Column(String(15), nullable=False, default="")
-    image = Column(String(500), nullable=False, default="")
-    href = Column(String(100), nullable=False, default="")
+# "Ресторан"
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(25))
+    photo = Column(String(500))
+    hours_of_operation = Column(String(10))
+    rating = Column(Integer)
+    dishes = relationship('RestaurantDish', back_populates='restaurant')
+    orders = relationship('RestaurantOrder', back_populates='restaurant')
 
-class diches(Base):
+# "Блюдо"
+class Dish(Base):
     __tablename__ = 'dishes'
-    id = Column(Integer, primary_key=True, default="", nullable=False)
-    weigth = Column(Integer, primary_key=True, default="", nullable=False)
-    price = Column(Integer, primary_key=True, default="", nullable=False)
-    discription = Column(String(500), primary_key=True, default="", nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50))
+    photo = Column(String(100))
+    price = Column(Integer)
+    weight = Column(Integer)
+    category = Column(String(50))
+    restaurants = relationship('RestaurantDish', back_populates='dish')
+    orders = relationship('OrderDish', back_populates='dish')
+
+# "Ресторан - Блюдо"
+class RestaurantDish(Base):
+    __tablename__ = 'restaurant_dishes'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    dish_id = Column(Integer, ForeignKey('dishes.id'))
+    restaurant = relationship('Restaurant', back_populates='dishes')
+    dish = relationship('Dish', back_populates='restaurants')
+
+# "Заказ"
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    restaurant_name = Column(String(25))
+    order_date = Column(DateTime, default=func.utcnow)
+    order_time = Column(String(10))
+    order_amount = Column(Float)
+    address = Column(String(255))
+    recipient_name = Column(String(50))
+    recipient_phone = Column(String(15))
+    dishes = relationship('OrderDish', back_populates='order')
+    restaurant = relationship('RestaurantOrder', back_populates='orders')
+
+# "Заказ - Блюдо"
+class OrderDish(Base):
+    __tablename__ = 'order_dishes'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    dish_id = Column(Integer, ForeignKey('dishes.id'))
+    quantity = Column(Integer)
+    dish = relationship('Dish', back_populates='orders')
+    order = relationship('OrderDish', back_populates='dishes')
+    
 
 
 def init_db():
@@ -31,16 +73,6 @@ def init_db():
     Base.metadata.create_all(bind=engine)
     db_session.commit()
 
-def print_schema(table_class):
-    from sqlalchemy.schema import CreateTable, CreateColumn
-    print(str(CreateTable(table_class.__table__).compile(db_engine)))
-
-def print_columns(table_class, *attrNames):
-   from sqlalchemy.schema import CreateTable, CreateColumn
-   c = table_class.__table__.c
-   print( ',\r\n'.join((str( CreateColumn(getattr(c, attrName)).compile(db_engine)) \
-                            for attrName in attrNames if hasattr(c, attrName)
-               )))
 
 if __name__ == "__main__":
     init_db()
