e35cd1d33d042265a166d183e6678078316c318a
Vitaliy Zhalalov <zhalalov9@mail.ru>
2024-03-24 21:39:19 +0300

    done downloading information from db in categories on website

--- app.py
+++ app.py
@@ -25,41 +25,31 @@ def index():
 def login():
     return render_template("login.html")
 
-# @app.route("/registration")
-# def registration():
-#     return render_template("registration.html")
-
 @app.route("/search")
 def search():
     return render_template("search_res.html")
 
-@app.route("/zotmanpizza")
-def zotman():
-    return render_template("zotmanpizza.html")
-
-@app.route("/burgerking")
-def burgerking():
-    return render_template("burgerking.html")
-
-@app.route("/vanvok")
-def vanvok():
-    return render_template("vanvok.html")
-
-@app.route("/mac")
-def mac():
-    return render_template("mac.html")
-
-@app.route("/kfc")
-def kfc():
-    return render_template("kfc.html")
-
-@app.route("/kebabnik")
-def kebabnik():
-    return render_template("kebabnik.html")
-
 @app.route("/oplata")
 def optala():
     return render_template("oplata.html")
+
+@app.route("/restaurant/<int:id>/")
+def restaurant_by_id(id):
+    from models import db_session, Restaurant, Dish
+    restaurant = db_session.query(Restaurant).filter(Restaurant.id == id).first()
+    if restaurant:
+        dishes = db_session.query(Dish).filter(Dish.rest_id == id).all()
+        dishes_by_category = {}
+        for dish in dishes:
+            category = dish.category
+            if category not in dishes_by_category:
+                dishes_by_category[category] = []
+            dishes_by_category[category].append(dish)
+        return render_template("mac.html", restaurant=restaurant, categories=dishes_by_category)
+    else:
+        return render_template("404.htm"), 404
+
+
 
 @manager.user_loader
 def load_user(user_id):
@@ -68,10 +58,4 @@ def load_user(user_id):
 
 
 if __name__ == "__main__":
-##    #Еще один способ добавления статической дирректории
-    from werkzeug.middleware.shared_data import SharedDataMiddleware
-    from controller import *
-    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-      '/': os.path.join(os.path.dirname(__file__), 'static')
-    })
     app.run(host='0.0.0.0', port=5000, debug=True)
